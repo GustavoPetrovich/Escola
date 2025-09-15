5af771af68876ff7f6a337fac5e6418748ddf692
gutorpetrovich@gmail.com <>
2025-09-15 20:18:04 -0300

    Atualização Escola.java
    atualização

--- src/main/java/com/mycompany/escola/Escola.java
+++ src/main/java/com/mycompany/escola/Escola.java
@@ -12,5 +12,7 @@ public class Escola {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Testanto Commit");
+        
     }
 }
